f0c6bdb19e98c678b0152fe3655c332bfc967a0d
Viktor Sermyazhko <VashaAkulka@mail.ru>
2024-03-31 16:06:03 +0300

    yes

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,16 @@
-DELETE student, mark FROM student INNER JOIN mark ON student.id = mark.student_id INNER JOIN subject ON mark.subject_id = subject.id WHERE subject.grade > 4;
+DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE EXISTS (SELECT 1 FROM subject WHERE subject.id = mark.subject_id AND subject.grade > 4));
 
-DELETE student, mark FROM student INNER JOIN mark ON student.id = mark.student_id WHERE mark < 4;
+DELETE FROM mark WHERE subject_id IN (SELECT id FROM subject WHERE grade > 4);
 
-DELETE FROM payment WHERE type_id IN (SELECT id FROM paymenttype WHERE name = 'Daily'); DELETE FROM paymenttype WHERE name = 'Daily';
+
+
+DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
+
+DELETE FROM mark WHERE mark < 4;
+
+
+
+DELETE FROM payment WHERE type_id IN (SELECT id FROM paymenttype WHERE name = 'Daily');
+DELETE FROM paymenttype WHERE name = 'Daily';
 
 DELETE FROM mark WHERE mark < 7;
--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SELECT min(student.birthday) from student;
+SELECT max(student.birthday) from student;
 SELECT min(payment.payment_date) from payment;
 
 SELECT avg(mark.mark) from mark inner join subject on subject.id = mark.subject_id where subject.name = 'Math';
